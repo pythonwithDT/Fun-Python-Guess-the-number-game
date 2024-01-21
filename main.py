6aa39017fa78a9325060473edffb72f82ea9b316
pythonwith_dt <emmanueltdortey@yahoo.com>
2024-01-21 20:18:46 +0000

    main.py

--- main.py
+++ main.py
@@ -27,4 +27,43 @@ if answer.lower() == "no":
     exit()
 else:
     print("I will think of a number, and you have to guess it")
-          
+    
+while True:
+    guess = True
+    while guess:
+        num = input("Type a number: ")
+        if num == "quit":
+            print("Thanks for playing")
+            exit()
+            
+        if num.isdigit():
+            print("Cool!")
+            num = int(num)
+            guess = False
+        else:
+            print("Incalid input! Try Again")
+            
+
+    secret = random.randit(1,num)
+ 
+    tries = None
+    count = 1
+ 
+    while tries != secret:
+        tries = input("Please type the guess between 1 and" + str(num) + ": ")
+        if tries.isdigit():
+            tries = int(tries)
+            
+        if tries == secret:
+            print("You Won")
+        else:
+            print("Please try again")
+            count += 1
+    print("It took you", count, "tries")
+    
+    
+        
+             
+    
+     
+        
